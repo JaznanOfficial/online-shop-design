8d33aaf19d166e2c8d5a1f5b2311a4f63b705a19
JaznanOfficial <jaznanofficial@gmail.com>
2022-01-03 20:19:56 +0600

    product chart done

--- src/Components/CardSlider/CardSlider.js
+++ src/Components/CardSlider/CardSlider.js
@@ -8,7 +8,7 @@ import './CardSlider.css'
 const CardSlider = () => {
     return (
         <div className="my-5">
-            <h1 className="mx-5 text-center">Chart 1</h1>
+            <h1 className="mx-5 text-center">Chart</h1>
             <Swiper
                 
                 slidesPerView={1.3}
@@ -19,35 +19,74 @@ const CardSlider = () => {
                 <SwiperSlide>
                 <Card className="m-5 product-card d-flex flex-row align-items-center justify-content-center">
                 <Card.Img variant="top" src="https://www.tods.com/fashion/tods/X1M11432960TYOB999/X1M11432960TYOB999-30.jpg?imwidth=865" />
-                <Card.Body>
+                <Card.Body className="my-5">
                   <Card.Title>Card title</Card.Title>
                   <Card.Text>
                     This is a longer card with supporting text below as a natural
-                    lead-in to additional content. This content is a little bit longer.
+                    lead-in to additional content
                             </Card.Text>
-                            <div className="d-flex flex-row align-items-center w-75 mx-auto card-bottom">
+                            <div className="d-flex flex-row align-items-center w-100 mx-auto card-bottom">
                                 <h3 className="text-danger">$ 250</h3>
                                 <Button variant="outline-success">Add to cart</Button>
                             </div>
                 </Card.Body>
               </Card>
                 </SwiperSlide>
+                
+                
                 <SwiperSlide>
                 <Card className="m-5 product-card d-flex flex-row align-items-center justify-content-center">
                 <Card.Img variant="top" src="https://www.tods.com/fashion/tods/X1M11432960TYOB999/X1M11432960TYOB999-30.jpg?imwidth=865" />
-                <Card.Body>
+                <Card.Body className="my-5">
                   <Card.Title>Card title</Card.Title>
                   <Card.Text>
                     This is a longer card with supporting text below as a natural
-                    lead-in to additional content. This content is a little bit longer.
+                    lead-in to additional content
                             </Card.Text>
-                            <div className="d-flex flex-row align-items-center w-75 mx-auto card-bottom">
+                            <div className="d-flex flex-row align-items-center w-100 mx-auto card-bottom">
                                 <h3 className="text-danger">$ 250</h3>
                                 <Button variant="outline-success">Add to cart</Button>
                             </div>
                 </Card.Body>
               </Card>
                 </SwiperSlide>
+                
+                
+                <SwiperSlide>
+                <Card className="m-5 product-card d-flex flex-row align-items-center justify-content-center">
+                <Card.Img variant="top" src="https://www.tods.com/fashion/tods/X1M11432960TYOB999/X1M11432960TYOB999-30.jpg?imwidth=865" />
+                <Card.Body className="my-5">
+                  <Card.Title>Card title</Card.Title>
+                  <Card.Text>
+                    This is a longer card with supporting text below as a natural
+                    lead-in to additional content
+                            </Card.Text>
+                            <div className="d-flex flex-row align-items-center w-100 mx-auto card-bottom">
+                                <h3 className="text-danger">$ 250</h3>
+                                <Button variant="outline-success">Add to cart</Button>
+                            </div>
+                </Card.Body>
+              </Card>
+                </SwiperSlide>
+                
+                
+                <SwiperSlide>
+                <Card className="m-5 product-card d-flex flex-row align-items-center justify-content-center">
+                <Card.Img variant="top" src="https://www.tods.com/fashion/tods/X1M11432960TYOB999/X1M11432960TYOB999-30.jpg?imwidth=865" />
+                <Card.Body className="my-5">
+                  <Card.Title>Card title</Card.Title>
+                  <Card.Text>
+                    This is a longer card with supporting text below as a natural
+                    lead-in to additional content
+                            </Card.Text>
+                            <div className="d-flex flex-row align-items-center w-100 mx-auto card-bottom">
+                                <h3 className="text-danger">$ 250</h3>
+                                <Button variant="outline-success">Add to cart</Button>
+                            </div>
+                </Card.Body>
+              </Card>
+                </SwiperSlide>
+                
                 
             </Swiper>
         </div>
